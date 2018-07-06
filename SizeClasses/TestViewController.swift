53e677c126d3f45500e2fde2fce383a930d1d9f7
xueyongwei <xueyongwei@ylmf.com>
2018-07-06 17:58:53 +0800

    demo code

---
+++ SizeClasses/TestViewController.swift
@@ -0,0 +1,36 @@
+
+//
+//  TestViewController.swift
+//  UIStackView
+//
+//  Created by 薛永伟 on 2018/7/6.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UIStackView
+//
+//  Created by 薛永伟 on 2018/7/6.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func onBtnClick(_ sender: UIButton) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
